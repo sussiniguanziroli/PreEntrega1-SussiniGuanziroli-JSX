13e310fbb415248ec4545ae135155e6aec3eee0c
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-05-16 11:23:53 -0300

    primer commit primera pre entrega

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { ItemListContainer } from './components/ItemListContainer'
+import { Header } from "./components/header/Header"
+import './css/main.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <ItemListContainer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Carrito.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Carrito = () => {
+  return (
+    <div className="cart">
+        <a className='cart-anchor' href="">🛒</a>
+        <small>1</small>
+    </div>
+  )
+}
